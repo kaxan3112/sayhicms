81f7ee137b8908b617beae3a5d562f5765cb1e29
khoa.td <khoa.td@tctav.com>
2016-02-24 18:36:49 +0700

    update template 002

--- templates/html/000002/js/common.js
+++ templates/html/000002/js/common.js
@@ -1,22 +1,8 @@
 $(document).ready(function() {
 
-  $("#partners-slider").owlCarousel({
-    autoPlay: 3000, //Set AutoPlay to 3 seconds
-    items : 6,
-    pagination : false,
-    itemsDesktop : [1199,3],
-    itemsDesktopSmall : [979,3]
-  });
-
-  $("#main-slider").owlCarousel({
-    autoPlay : 3000,
-    stopOnHover : false,
-    navigation:false,
-    paginationSpeed : 1000,
-    goToFirstSpeed : 2000,
-    singleItem : true,
-    autoHeight : true,
-    transitionStyle:"fade"
-  });
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("body").toggleClass("toggled");
+    });
 
 });
---
+++ templates/html/000002/js/top.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $("#main-slider").owlCarousel({
+    autoPlay : 3000,
+    stopOnHover : false,
+    navigation : false,
+    pagination : false,
+    goToFirstSpeed : 2000,
+    singleItem : true,
+    autoHeight : true,
+    transitionStyle:"fade"
+  });
+
+});
